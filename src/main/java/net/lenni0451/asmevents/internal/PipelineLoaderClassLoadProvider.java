e31157048151ebdf0d72cf14d80e89a3d836a8e5
Lenni0451 <lenni0451@laptop>
2021-05-14 20:04:10 +0200

    Added parentClass field to PipelineLoaderClassLoadProvider

--- src/main/java/net/lenni0451/asmevents/internal/PipelineLoaderClassLoadProvider.java
+++ src/main/java/net/lenni0451/asmevents/internal/PipelineLoaderClassLoadProvider.java
@@ -1,14 +1,19 @@
 package net.lenni0451.asmevents.internal;
 
-import net.lenni0451.asmevents.EventManager;
 import net.lenni0451.asmevents.IClassLoadProvider;
 import net.lenni0451.asmevents.utils.PipelineLoader;
 
 public class PipelineLoaderClassLoadProvider implements IClassLoadProvider {
 
+    private final Class<?> parentClass;
+
+    public PipelineLoaderClassLoadProvider(final Class<?> parentClass) {
+        this.parentClass = parentClass;
+    }
+
     @Override
     public <T> Class<T> loadClass(String name, byte[] data) {
-        return new PipelineLoader(EventManager.class).loadPipeline(name, data);
+        return new PipelineLoader(this.parentClass).loadPipeline(name, data);
     }
 
 }
