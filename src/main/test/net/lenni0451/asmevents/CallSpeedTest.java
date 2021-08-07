69dc1bb5f41a4617e54a44cc507fdf380d1fc96b
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2021-08-07 18:30:33 +0200

    Now calling speed test 10 times before the actual test to let the jvm do some speed improvement magic

--- src/main/test/net/lenni0451/asmevents/CallSpeedTest.java
+++ src/main/test/net/lenni0451/asmevents/CallSpeedTest.java
@@ -3,39 +3,47 @@ package net.lenni0451.asmevents;
 import net.lenni0451.asmevents.event.EventTarget;
 import net.lenni0451.asmevents.event.IEvent;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.text.DecimalFormat;
 import java.util.HashMap;
 
 public class CallSpeedTest {
 
-    public static void main(String[] args) throws Throwable {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) run(new PrintStream(new ByteArrayOutputStream()));
+        run(System.out);
+    }
+
+    public static void run(PrintStream out) {
         EventManager.setErrorListener(Throwable::printStackTrace);
         DecimalFormat df = new DecimalFormat();
+        EventListener listener = new EventListener();
 
-        System.out.println("---------- Register Event Listener ----------");
+        out.println("---------- Register Event Listener ----------");
         long registerTime;
         {
             registerTime = System.nanoTime();
-            EventManager.register(new EventListener());
-            System.out.println("EventManager: " + df.format(System.nanoTime() - registerTime));
+            EventManager.register(listener);
+            out.println("EventManager: " + df.format(System.nanoTime() - registerTime));
         }
-        System.out.println();
+        out.println();
 
         long start;
         float middle;
-        System.out.println("---------- Single call ----------");
+        out.println("---------- Single call ----------");
         {
             start = System.nanoTime();
             EventManager.call(new ExampleEvent1());
-            System.out.println("EventManager (ExampleEvent1): " + df.format(System.nanoTime() - start));
+            out.println("EventManager (ExampleEvent1): " + df.format(System.nanoTime() - start));
 
             start = System.nanoTime();
             EventManager.call(new ExampleEvent2());
-            System.out.println("EventManager (ExampleEvent2): " + df.format(System.nanoTime() - start));
+            out.println("EventManager (ExampleEvent2): " + df.format(System.nanoTime() - start));
         }
-        System.out.println();
+        out.println();
 
-        System.out.println("---------- 1000 call ----------");
+        out.println("---------- 1000 call ----------");
         {
             middle = 0;
             for (int i = 0; i < 1000; i++) {
@@ -44,7 +52,7 @@ public class CallSpeedTest {
                 middle += System.nanoTime() - start;
             }
             middle /= 1000F;
-            System.out.println("EventManager (ExampleEvent1): " + df.format(middle));
+            out.println("EventManager (ExampleEvent1): " + df.format(middle));
 
             middle = 0;
             for (int i = 0; i < 1000; i++) {
@@ -53,11 +61,11 @@ public class CallSpeedTest {
                 middle += System.nanoTime() - start;
             }
             middle /= 1000F;
-            System.out.println("EventManager (ExampleEvent2): " + df.format(middle));
+            out.println("EventManager (ExampleEvent2): " + df.format(middle));
         }
-        System.out.println();
+        out.println();
 
-        System.out.println("---------- 100000 call ----------");
+        out.println("---------- 100000 call ----------");
         {
             middle = 0;
             for (int i = 0; i < 100000; i++) {
@@ -66,7 +74,7 @@ public class CallSpeedTest {
                 middle += System.nanoTime() - start;
             }
             middle /= 100000F;
-            System.out.println("EventManager (ExampleEvent1): " + df.format(middle));
+            out.println("EventManager (ExampleEvent1): " + df.format(middle));
 
             middle = 0;
             for (int i = 0; i < 100000; i++) {
@@ -75,8 +83,18 @@ public class CallSpeedTest {
                 middle += System.nanoTime() - start;
             }
             middle /= 100000F;
-            System.out.println("EventManager (ExampleEvent2): " + df.format(middle));
+            out.println("EventManager (ExampleEvent2): " + df.format(middle));
         }
+        out.println();
+
+        out.println("---------- UnRegister Event Listener ----------");
+        long unregisterTime;
+        {
+            unregisterTime = System.nanoTime();
+            EventManager.unregister(listener);
+            out.println("EventManager: " + df.format(System.nanoTime() - unregisterTime));
+        }
+        out.println();
     }
 
     public static class ExampleEvent1 implements IEvent {
